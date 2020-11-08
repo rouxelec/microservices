66e7a9e4020d1514dd89ef5509d1f5483e869160
Francois <francois.rouxel@slalom.com>
2020-11-07 21:11:21 -0500

    codebuild

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -16,6 +16,7 @@ module "codebuild" {
   buildspec                   = "src/codebuild/build_base.yaml"
   artifact_type               = "NO_ARTIFACTS"
   private_repository          = "true"
+  build_image                 = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
 }
 
 provider "aws" {
