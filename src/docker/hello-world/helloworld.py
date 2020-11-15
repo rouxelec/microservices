2a24117cf49648b4f4eeadae27019f371f082855
Francois <francois.rouxel@slalom.com>
2020-11-15 16:01:24 -0500

    added app

--- src/docker/hello-world/helloworld.py
+++ src/docker/hello-world/helloworld.py
@@ -1,13 +1,7 @@
-from flask import Flask, render_template
-import os
-import random
-
+from flask import Flask
 app = Flask(__name__)
-
-
 @app.route("/")
-def index():
-    return "Hello, World!"
-
+def hello():
+    return "Hello World!"
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
