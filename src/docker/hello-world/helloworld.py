7260885f321a2398272bb2349c2add5cbf2374aa
Francois <francois.rouxel@slalom.com>
2020-12-05 15:38:06 -0500

    added dynamodb

--- src/docker/hello-world/helloworld.py
+++ src/docker/hello-world/helloworld.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 dynamodb=None
 @app.route("/")
 def hello():
-    return "Hello World v3!"
+    return "Hello World v4!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
