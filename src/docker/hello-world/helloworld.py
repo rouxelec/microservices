ac6725f184af46ebce838a4842aa79329f4402e2
Francois <francois.rouxel@slalom.com>
2020-12-05 15:24:07 -0500

    added dynamodb

--- src/docker/hello-world/helloworld.py
+++ src/docker/hello-world/helloworld.py
@@ -9,8 +9,8 @@ def hello():
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
     try:
-      table = dynamodb.Table('GameScores')
-      table.put_item(Item={"UserId":"Francois","Score":10})
+        table = dynamodb.Table('GameScores')
+        table.put_item(Item={"UserId":"Francois","Score":10})
     except:
         print("Unexpected error:", sys.exc_info()[0])
     return "Hello World v3!"
