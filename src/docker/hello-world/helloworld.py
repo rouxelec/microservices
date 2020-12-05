7f4dfdda27b3edfc16d379ce0366e1add7c5a3d8
Francois <francois.rouxel@slalom.com>
2020-12-05 15:18:36 -0500

    added dynamodb

--- src/docker/hello-world/helloworld.py
+++ src/docker/hello-world/helloworld.py
@@ -3,9 +3,9 @@ import sys
 from flask import Flask
 
 app = Flask(__name__)
+dynamodb=None
 @app.route("/")
 def hello():
-    dynamodb=None
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
     try:
@@ -13,7 +13,6 @@ def hello():
       table.put_item(Item={"UserId":"Francois","Score":10})
     except:
         print("Unexpected error:", sys.exc_info()[0])
-        return "Unexpected error:", sys.exc_info()[0]
     return "Hello World v3!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
