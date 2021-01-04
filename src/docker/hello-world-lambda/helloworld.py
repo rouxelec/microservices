83e2394cdc5eddade3c9a51dba2aa86047b751f0
Francois <francois.rouxel@slalom.com>
2021-01-04 11:34:19 -0500

    added lambda container

--- src/docker/hello-world-lambda/helloworld.py
+++ src/docker/hello-world-lambda/helloworld.py
@@ -26,4 +26,4 @@ def healthcheck():
     return "HealthCheck!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=int("80"), debug=True)
