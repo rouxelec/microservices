f653e9ecf6d0e23147724eed085b611b31392991
Francois <francois.rouxel@slalom.com>
2021-02-07 14:53:29 -0500

    adding testing

--- src/codebuild/test.sh
+++ src/codebuild/test.sh
@@ -1,7 +1,7 @@
 for i in $(seq 1 60); do
     echo $i
     curl $1
-    sleep(1)
+    sleep 1
     RESULT=$(aws dynamodb get-item --table-name Deployment --key '{"UserId":{"S":"ec2"}}')
     echo $RESULT
 done
