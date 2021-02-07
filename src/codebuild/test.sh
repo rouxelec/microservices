7d6c0a0cf800222caf2fdeab1ae81bcc600d1a4b
Francois <francois.rouxel@slalom.com>
2021-02-07 18:21:48 -0500

    test

--- src/codebuild/test.sh
+++ src/codebuild/test.sh
@@ -1,14 +1,13 @@
-for i in $(seq 1 1); do
+for i in $(seq 1 2); do
     echo $i
     curl $1
     sleep 1
     RESULT1=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"$2"}}' | jq -r '.Item.Version.S')
     echo $RESULT1
     RESULT2=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"api_version"}}' | jq -r '.Item.Version.S')
-    echo $RESULT1
+    echo $RESULT2
     if [ "$RESULT1" == "$RESULT2" ]
     then
-        echo Hey that\'s a large number.
         break;
     fi
 done
