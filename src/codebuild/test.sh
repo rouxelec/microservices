013aa07278ab3f6460923a2e2ae65e300041fbfe
Francois <francois.rouxel@slalom.com>
2021-02-14 08:53:29 -0500

    echo test.sh

--- src/codebuild/test.sh
+++ src/codebuild/test.sh
@@ -1,14 +1,15 @@
 for i in $(seq 1 2); do
     echo $i
     curl $1
-    echo first param
+    echo first param:
     echo $1
-    echo second param
+    echo second param:
     echo $2
-    sleep 1
     RESULT1=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"$2"}}' | jq -r '.Item.Version.S')
+    echo result1:
     echo $RESULT1
     RESULT2=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"api_version"}}' | jq -r '.Item.Version.S')
+    echo result2:
     echo $RESULT2
     if [ "$RESULT1" == "$RESULT2" ]
     then
