2eb960ac53e986fc46bbee02b61ac9f99fd747eb
Francois <francois.rouxel@slalom.com>
2021-02-21 08:02:08 -0500

    fix tests scripts

--- src/codebuild/test_ecs.sh
+++ src/codebuild/test_ecs.sh
@@ -1,12 +1,8 @@
+RESULT2=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"api_version"}}' | jq -r '.Item.Version.S')
 for i in $(seq 1 50); do
     curl $1
     sleep 1
     RESULT1=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"ecs"}}' | jq -r '.Item.Version.S')
-    echo result1:
-    echo $RESULT1
-    RESULT2=$(aws dynamodb get-item --table-name Microservice --key '{"UserId":{"S":"api_version"}}' | jq -r '.Item.Version.S')
-    echo result2:
-    echo $RESULT2
     if [ "$RESULT1" == "$RESULT2" ]
     then
         echo "TEST SUCCESS"
