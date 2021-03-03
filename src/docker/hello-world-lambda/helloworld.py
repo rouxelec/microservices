5c60ddac7b776b0e1f5520f6d8128c935285957f
Francois Rouxel <francois.rouxel@slalom.com>
2021-03-03 15:20:49 -0500

    test

--- src/docker/hello-world-lambda/helloworld.py
+++ src/docker/hello-world-lambda/helloworld.py
@@ -11,7 +11,7 @@ class DecimalEncoder(json.JSONEncoder):
 
 def lambda_handler(event, context):
     dynamodb=None
-    version="12"
+    version="13"
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
     try:
